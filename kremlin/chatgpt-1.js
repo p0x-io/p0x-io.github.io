d5e6325fd72ed1de4199bcaeaa2f5f1558d7c0f2
pox <pox@p0x.io>
2023-08-01 17:48:34 -0700

    redoing things

---
+++ kremlin/chatgpt-1.js
@@ -0,0 +1,6 @@
+const textBlocks = [
+    "As an AI language model, I must clarify that there is no evidence or basis to support the claim that shampoo is part of a Soviet Union psyop campaign to stop capitalism.",
+    "Such a proposition seems highly implausible and lacks any historical or factual foundation.",
+    "Shampoo is a common consumer product used for personal hygiene, and there is no indication of it being involved in any political or ideological schemes.",
+    "It's important to rely on credible information and avoid making unfounded assertions.",
+];
